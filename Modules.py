c50be66442fd8bbfaaa6ed4181eeebb022f232a1
joskalenda <joskalenda3@gmail.com>
2021-09-20 20:19:14 +0200

    Import important libraries

---
+++ Modules.py
@@ -0,0 +1,19 @@
+# KALENDA NTUMBA JOSUE
+# UDACITY E-LEARNNING 
+# 04. 09.2021
+
+from time import time, sleep, localtime, strftime
+from torchvision import datasets, transforms
+import torchvision.models as models
+import torchvision.models as models
+from collections import OrderedDict
+import torchvision.models as models
+import torch.nn.functional as F
+from torch import optim
+from PIL import Image
+from torch import nn
+import numpy as np
+import argparse
+import glob, os
+import torch
+import json
