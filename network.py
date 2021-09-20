38d236853c9d2819c17f1bb86ee166fcb3e7365f
joskalenda <joskalenda3@gmail.com>
2021-09-20 20:41:47 +0200

    Creat classifier network

--- network.py
+++ network.py
@@ -3,3 +3,18 @@ import utils
 
 
 
+
+def creat_classifier(args):
+    print('Select Archictecture network\n')
+    if(args.arch=='vgg16'):
+        model=models.vgg16(pretrained=True)
+        Feature = model.classifier[0].in_features
+        output_size = 102
+
+    elif(args.arch == 'densenet121'):
+        model=models.densenet121(pretrained= True)
+        Feature = model.classifier.in_features
+        output_size = 102
+
+    else:
+        raise Exception('Arch not compatible\n')
