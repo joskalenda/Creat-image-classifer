cce3bb78c6bb8099158d48f4474be82b3e79403c
joskalenda <joskalenda3@gmail.com>
2021-09-20 20:59:48 +0200

    Define model function and set the model to gpu/cpu

--- network.py
+++ network.py
@@ -23,7 +23,7 @@ def creat_classifier(args):
 
     for parm in model.parameters():
         parm.requires_grad=False;
-        
+
     # Features = model.classifier[0].in_features
     Classifier= nn.Sequential(OrderedDict([('dropout1', nn.Dropout(p=0.3)),
                                             ('fc1', nn.Linear(Feature, args.hidden_units)),
@@ -38,3 +38,23 @@ def creat_classifier(args):
     model.class_to_idx = train_dataset.class_to_idx
     criterion = nn.NLLLoss()
     optimizer= optim.Adam(model.classifier.parameters(),lr=args.lr)
+
+
+
+def Model_function(args,model,optimizer,criterion,trainloader,valloader,epochs,device):
+    
+    print("Starting the Traing and calculate the time\n")
+    start_time = time()
+#     args = Arg_function()
+    if(args.gpu):
+        if torch.cuda.is_available():
+            device = 'gpu'
+        else:
+            device = 'cpu'
+    else:
+        device = 'cpu'
+      
+    model.to(device)
+    accuracy = 0
+    model.optim_state_dict = optimizer.stat
+
