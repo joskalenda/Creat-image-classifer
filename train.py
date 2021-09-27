1dd49abcefc02b5ae07757e3dd8c8c0a953b8ef5
joskalenda <joskalenda3@gmail.com>
2021-09-27 21:35:50 +0200

    Define the trainfunction

--- train.py
+++ train.py
@@ -1,3 +1,15 @@
 from Modules import *
 import utils
 import network
+
+
+def Tr_function(args):
+    if(args.gpu):
+        if torch.cuda.is_available():
+            device = 'gpu'
+            print('cuda available')
+        else:
+            device = 'cpu'
+            print('Cuda not available, train with cpu')
+    else:
+        device = 'cpu'
