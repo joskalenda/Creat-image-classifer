c3b27492fff9351ce0dcffb1bf73f446ab2e2b58
joskalenda <joskalenda3@gmail.com>
2021-09-27 17:45:19 +0200

    Creat predict function and set the model to device

--- predict.py
+++ predict.py
@@ -1,3 +1,17 @@
 from Modules import*
 import utils
 import network
+
+
+def LoadPredict(args):
+ 
+    model=network.loadCheckpoint(args.checkpoint_path)
+
+    if(in_arg.gpu):
+        if torch.cuda.is_available():
+            device = 'gpu'
+        else:
+            device = 'cpu'
+    else:
+        device = 'cpu'
+    model.to(device)
