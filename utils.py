a4583fde7ab9263316018de5d2e2b9a24c0f7bd0
joskalenda <joskalenda3@gmail.com>
2021-09-27 22:10:14 +0200

    Creat a function to processs the image

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
 from Modules import  *
+
+def Imageprocessing(image):
+    
+    im = Image.open(image)
+
+    # Define transforms
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
