0b5071a9d194d6b196d4f6842319d9394ce5958f
joskalenda <joskalenda3@gmail.com>
2021-09-27 22:11:34 +0200

    Process the image and return a numpy array

--- utils.py
+++ utils.py
@@ -11,3 +11,7 @@ def Imageprocessing(image):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
+
+        # Preprocess the image
+    img_tensor = preprocess(im)
+    return img_tensor.numpy()
