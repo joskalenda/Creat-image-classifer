f37651b867b7d3d7794c99e0f8eefcbe9d768d09
joskalenda <joskalenda3@gmail.com>
2021-09-27 21:38:00 +0200

    Define data transforms

--- train.py
+++ train.py
@@ -13,3 +13,24 @@ def Tr_function(args):
             print('Cuda not available, train with cpu')
     else:
         device = 'cpu'
+
+    
+    # TODO:task1 DEFINE DATA TRANSFORM, DEFINING DATASETS 
+
+    test_transform = transforms.Compose([transforms.Resize(256),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
+                                    ])
+    train_transform = transforms.Compose([transforms.RandomRotation(30),
+                                        transforms.RandomResizedCrop(224),
+                                        transforms.RandomHorizontalFlip(),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
+                                        ])
+
+    val_transform = transforms.Compose([transforms.Resize(256),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
+                                       ])
